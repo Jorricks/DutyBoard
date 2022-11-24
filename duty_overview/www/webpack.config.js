6af9481a4ab1e5ac59457179b401e6fa497a6558
jorricks <jorricks3@gmail.com>
2022-11-24 11:01:42 +0100

    Add required webpack package

--- duty_overview/www/webpack.config.js
+++ duty_overview/www/webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
              "presets": [
                "@babel/preset-env",
                "@babel/preset-flow",
-               ["@babel/preset-react", {"runtime": "automatic"}]
+               ["@babel/preset-react", {"runtime": "automatic"}],
+               "@babel/preset-typescript",
              ]
           },
         },
