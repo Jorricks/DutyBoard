2a479858b694b1f600d3402b8d3f23bd7797c8b8
jorrick <jorrick.sleijster@adyen.com>
2025-04-15 12:49:25 +0200

    🐛 Calendar events might be out-of-order when events are modified

--- duty_board/alchemy/api_queries.py
+++ duty_board/alchemy/api_queries.py
@@ -62,7 +62,7 @@ def get_calendars(session: SASession, all_encountered_person_uids: Set[int], tim
             last_update=format_datetime_for_timezone(single_calendar.last_update_utc, timezone),
             error_msg=single_calendar.error_msg or "",
             sync=single_calendar.sync,
-            events=events.get(single_calendar.uid) or [],
+            events=sorted((events.get(single_calendar.uid) or []), key=lambda event: event.start_event),
         )
         for single_calendar in result
     ]
